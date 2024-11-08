dffab5837db8d6cbb8a2d31d21d1b9411bb13f09
Joan Nauroy <jnauroy@2G6.42angouleme.fr>
2024-11-08 17:58:46 +0100

    checker le temp ou reecrire string

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jnauroy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 13:52:34 by jnauroy           #+#    #+#             */
-/*   Updated: 2024/11/07 18:17:06 by jnauroy          ###   ########.fr       */
+/*   Updated: 2024/11/08 17:44:48 by jnauroy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,41 @@
 
 char	*init_string(char *string)
 {
-	char *temp;
+	char 	*temp;
 
-	temp = NULL;
 	if (string)
 	{
 		temp = ft_strchr(string, '\n');
 		free(string);
-		string = calloc(sizeof(char), 1);
+		if (!temp || !i)
+			return (0);
+		string = ft_calloc(sizeof(char), 1);
+		if (!string)
+			return (0);
 		string = ft_gnljoin(string, temp);
 		return (string);
 	}
-	string = calloc(sizeof(char), 1);
+	string = ft_calloc(sizeof(char), 1);
 	if (!string)
 		return (0);
 	return (string);
 }
 
-int	check_for_bsn(char *str)
+int	check_for_bsn(char *str, int size)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (i < ft_strlen(str))
 	{
 		if (str[i] == '\n')
 			return (0);
 		i++;
 	}
+	if (size == -1)
+		return (1);
+	if (size < BUFFER_SIZE && str[i] == '\0')
+		return (0);
 	return (1);
 }
 
@@ -55,13 +62,13 @@ char	*ft_create_str(char *src)
 	n = 0;
 	while (src[n] != '\0' && src[n] != '\n')
 		n++;
-	if (src[n] == '\0')
-		n--;
-	dest = malloc(sizeof(char) * (n + 1));
+	if (src[n] == '\n')
+		n++;
+	dest = malloc(sizeof(char) * n);
 	if (!dest)
 		return (0);
 	i = 0;
-	while (i <= n)
+	while (i < n)
 	{
 		dest[i] = src[i];
 		i++;
@@ -74,23 +81,25 @@ char	*get_next_line(int fd)
 {
 	char		*buffer;
 	static char *string;
-	int			size;
 	char		*sentence;
+	int			size;
 	
 	string = init_string(string);
 	if (!string)
 		return (0);
-	buffer = NULL;
-	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	size = BUFFER_SIZE;
-	while (check_for_bsn(string))
+	size = -1;
+	while (check_for_bsn(string, size))
 	{
+		buffer = NULL;
+		buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!buffer)
+			return (0);
 		size = read(fd, buffer, BUFFER_SIZE);
 		if (size == -1)
 			return (0);
 		string = ft_gnljoin(string, buffer);
+		free(buffer);
 	}
 	sentence = ft_create_str(string);
-	free(buffer);
 	return (sentence);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jnauroy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 14:14:09 by jnauroy           #+#    #+#             */
-/*   Updated: 2024/11/07 18:10:24 by jnauroy          ###   ########.fr       */
+/*   Updated: 2024/11/08 09:40:15 by jnauroy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ int	main(void)
 	int	fd;
 	char *line;
 
-	line = "0";
 	fd = open("theclash.txt", O_RDONLY);
-	while (line != NULL) 
+	do 
 	{
 		line = get_next_line(fd);
 		printf("%s", line);
 		free(line);
-	}
+	} while (line);
+	close (fd);
 }
